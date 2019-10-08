d95bec0c478707af336f23ab737c1fb7a2a1c24c
Brad Duthie <brad.duthie@gmail.com>
2019-10-08 16:16:28 +0100

    Add sample code

---
+++ sample_code.R
@@ -0,0 +1,21 @@
+2 + 5; 
+4 * 4;  
+12 - 3; 
+5^2;
+sqrt(256);
+log(x = 256, base = 10);
+log(x = 256);
+log(256);
+log( log(256) );
+help(topic = log);
+?log;
+var_1 <- 10;
+var_1 * 5; # Multiplying the variable by 5
+vector_1 <- c(5, 1, 3, 5, 7, 11); # Six numbers
+vector_1;      # Prints out the vector
+vector_1 + 10; # Prints the vector elements plus ten
+vector_1 * 2;  # Prints the vector elements times two
+vector_1 <- c(vector_1, 31, 100); # Appends the vector
+vector_1;
+object_1 <- list(vector_1, 54, "string of words");
+object_1;
