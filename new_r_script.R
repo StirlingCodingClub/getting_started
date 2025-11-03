c6010123fb8d36147512e6f98b152b4be6c5be14
bradduthie <brad.duthie@gmail.com>
2025-11-03 00:30:37 +0000

    Update notes

---
+++ new_r_script.R
@@ -0,0 +1,33 @@
+log(x = 1000000, base = 10) # Want to get base 10 log of 1000000
+
+# How to assign variable
+var_1 <- 10
+var_2 = 20
+30 -> var_3
+
+sum_variables <- var_1 + var_2 + var_3
+
+var_four <- "the quick brown fox"
+
+my_vector <- c(3, 6, 7, var_1)
+my_vector * 10
+
+my_list <- list(my_vector, var_four, 9)
+
+
+my_matrix <- matrix(data = 1:16, nrow = 4);
+
+
+
+dat <- read.csv(file = "Bumpus_data.csv", header = TRUE)
+
+hist(dat$totlen, main = "my histogram", cex.lab = 1.5)
+
+
+
+
+
+
+
+
+
